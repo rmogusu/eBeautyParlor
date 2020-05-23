eba35cd822175eb56853829d089c26e84bf31421
rmogusu <rmogusu123@gmail>
2020-05-23 21:23:55 +0200

    Add test to assert that location is entered through a form .

--- app/src/androidTest/java/com/moringaschool/ebeautyparlor/MainActivityInstrumentationTest.java
+++ app/src/androidTest/java/com/moringaschool/ebeautyparlor/MainActivityInstrumentationTest.java
@@ -6,6 +6,8 @@ import org.junit.Rule;
 import org.junit.Test;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
@@ -15,6 +17,26 @@ public class MainActivityInstrumentationTest {
     @Rule
     public ActivityTestRule<MainActivity> activityTestRule =
             new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void validateEditText() {
+        onView(withId(R.id.locationEditText)).perform(typeText("Nairobi"))
+                .check(matches(withText("Nairobi")));
+    }
+
+    @Test
+    public void locationIsSentToPetsActivity() {
+        String location = "Nairobi";
+        onView(withId(R.id.locationEditText)).perform(typeText(location)).perform(closeSoftKeyboard());
+        try {
+            Thread.sleep(250);
+        } catch (InterruptedException e) {
+            System.out.println("got interrupted!");
+        }
+        onView(withId(R.id.findParlorButton)).perform(click());
+        onView(withId(R.id.locationTextView)).check(matches
+                (withText("Here are all the beauty parlor  near: " + location)));
+    }
 
     @Test
     public void validateEditText() {
