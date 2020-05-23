47d2daf85d4fa2cdaa9d17c24e2ebce88f80bcf8
rmogusu <rmogusu123@gmail>
2020-05-23 13:09:05 +0200

    Add on click lister to the button.

--- app/src/main/java/com/moringaschool/ebeautyparlor/MainActivity.java
+++ app/src/main/java/com/moringaschool/ebeautyparlor/MainActivity.java
@@ -8,15 +8,18 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
-    @BindView(R.id.findPetsButton) Button mFindPetsButton;
+    @BindView(R.id.findParlorsButton) Button mFindParlorButton;
     @BindView(R.id.locationEditText) EditText mLocationEditText;
     @BindView(R.id.appNameTextView) TextView mAppNameTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        mFindParlorButton.setOnClickListener(this);
 
     }
 }
