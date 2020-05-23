01d35eb5c39634368229715c546540e2c53ec411
rmogusu <rmogusu123@gmail>
2020-05-23 13:43:53 +0200

    Add code to oncreate to ensure location is created successfully.

--- app/src/main/java/com/moringaschool/ebeautyparlor/ParlorActivity.java
+++ app/src/main/java/com/moringaschool/ebeautyparlor/ParlorActivity.java
@@ -3,6 +3,11 @@ package com.moringaschool.ebeautyparlor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class ParlorActivity extends AppCompatActivity {
 
@@ -10,5 +15,13 @@ public class ParlorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parlor);
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String pet = ((TextView)view).getText().toString();
+                Log.v(TAG, "In the onItemClickListener!");
+                Toast.makeText(PetsActivity.this, pet , Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
