77365acc493f3fe57981722c78c497282610b646
rmogusu <rmogusu123@gmail>
2020-05-23 13:06:57 +0200

    Add button toast.

--- app/src/main/java/com/moringaschool/ebeautyparlor/MainActivity.java
+++ app/src/main/java/com/moringaschool/ebeautyparlor/MainActivity.java
@@ -3,12 +3,20 @@ package com.moringaschool.ebeautyparlor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.BindView;
 
 public class MainActivity extends AppCompatActivity {
-
+    @BindView(R.id.findPetsButton) Button mFindPetsButton;
+    @BindView(R.id.locationEditText) EditText mLocationEditText;
+    @BindView(R.id.appNameTextView) TextView mAppNameTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
