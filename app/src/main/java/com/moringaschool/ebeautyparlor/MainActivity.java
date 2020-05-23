540c76f041987f8466cafd761e13e02a7634e01a
rmogusu <rmogusu123@gmail>
2020-05-23 13:11:07 +0200

    Add an intent to open new activity.

--- app/src/main/java/com/moringaschool/ebeautyparlor/MainActivity.java
+++ app/src/main/java/com/moringaschool/ebeautyparlor/MainActivity.java
@@ -2,10 +2,13 @@ package com.moringaschool.ebeautyparlor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -21,5 +24,15 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         mFindParlorButton.setOnClickListener(this);
 
+    }
+    @Override
+    public void onClick(View v) {
+        if (v == mFindPetsButton) {
+            String location = mLocationEditText.getText().toString();
+            Intent intent = new Intent(MainActivity.this, PetsActivity.class);
+            intent.putExtra("location", location);
+            startActivity(intent);
+            Toast.makeText(MainActivity.this, location, Toast.LENGTH_LONG).show();
+        }
     }
 }
