deb068e5891c565ef13ae84fcaa35e4b0ee6d97d
rmogusu <rmogusu123@gmail>
2020-05-23 13:47:36 +0200

    Add a view to the parlor activity.

--- app/src/main/java/com/moringaschool/ebeautyparlor/ParlorActivity.java
+++ app/src/main/java/com/moringaschool/ebeautyparlor/ParlorActivity.java
@@ -6,11 +6,16 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class ParlorActivity extends AppCompatActivity {
+import butterknife.BindView;
 
+public class ParlorActivity extends AppCompatActivity {
+    public static final String TAG = ParlorActivity.class.getSimpleName();
+    @BindView(R.id.locationTextView) TextView mLocationTextView;
+    @BindView(R.id.listView) ListView mListView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,7 +25,7 @@ public class ParlorActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 String pet = ((TextView)view).getText().toString();
                 Log.v(TAG, "In the onItemClickListener!");
-                Toast.makeText(PetsActivity.this, pet , Toast.LENGTH_LONG).show();
+                Toast.makeText(ParlorActivity .this, pet , Toast.LENGTH_LONG).show();
             }
         });
     }
