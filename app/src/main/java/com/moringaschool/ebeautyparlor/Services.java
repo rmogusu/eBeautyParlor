cd80086b8754c6eff87b1588c0ac5745ee7fc3b6
rmogusu <rmogusu123@gmail>
2020-05-29 15:53:56 +0200

    create a new OkHttpClient to create and send our request.

--- app/src/main/java/com/moringaschool/ebeautyparlor/Services.java
+++ app/src/main/java/com/moringaschool/ebeautyparlor/Services.java
@@ -2,13 +2,25 @@ package com.moringaschool.ebeautyparlor;
 
 
 import okhttp3.Callback;
+import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
 
 public class Services {
     public static void findBeautyParlor(String location, Callback callback) {
         OkHttpClient client = new OkHttpClient.Builder()
                 .build();
+        HttpUrl.Builder urlBuilder = HttpUrl.parse(Constants.BASE_URL).newBuilder();
+        urlBuilder.addQueryParameter(Constants.LOCATION_QUERY_PARAMETER, location);
+        String url = urlBuilder.build().toString();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .header("Authorization", Constants.API_KEY)
+                .build();
+
     }
-
-
 }
+
+
+
