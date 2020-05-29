a4924fa2e3d5c1c7b84bd736bd737a866e1556dd
rmogusu <rmogusu123@gmail>
2020-05-29 14:47:39 +0200

    configure network connection.

---
+++ app/src/main/java/com/moringaschool/ebeautyparlor/Constants.java
@@ -0,0 +1,6 @@
+package com.moringaschool.ebeautyparlor;
+
+public class Constants {
+    public static final String BASE_URL = "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest";
+    public static final String API_KEY = BuildConfig.API_KEY;
+}
---
+++ app/src/main/java/com/moringaschool/ebeautyparlor/ParlorListAdapter.java
@@ -0,0 +1,9 @@
+package com.moringaschool.ebeautyparlor;
+
+import android.content.Context;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ParlorListAdapter {
+
+}
