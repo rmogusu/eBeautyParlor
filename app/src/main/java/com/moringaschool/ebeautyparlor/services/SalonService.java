3fe2edced7a46700d736f8abb3cfd8cbe2c10413
rmogusu <rmogusu123@gmail>
2020-05-31 12:45:35 +0200

    Add a thread ui.

--- app/src/main/java/com/moringaschool/ebeautyparlor/services/SalonService.java
+++ app/src/main/java/com/moringaschool/ebeautyparlor/services/SalonService.java
@@ -1,12 +1,21 @@
 package com.moringaschool.ebeautyparlor.services;
 
 import com.moringaschool.ebeautyparlor.Constants;
+import com.moringaschool.ebeautyparlor.models.BeautyParlor;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.Response;
 
 public class SalonService {
     public static void findBeautyParlor(String location, Callback callback) {
@@ -26,6 +35,43 @@ public class SalonService {
         Call call = client.newCall(request);
         call.enqueue(callback);
    }
-
+    public ArrayList<BeautyParlor> processResults(Response response){
+        ArrayList<BeautyParlor> beautyParlors = new ArrayList<>();
+        try{
+            String jsonData = response.body().string();
+            JSONObject salonJSON = new JSONObject(jsonData);
+            JSONArray businessesJSON = salonJSON.getJSONArray("businesses");
+            if (response.isSuccessful()){
+                for (int i = 0; i < businessesJSON.length(); i++){
+                    JSONObject beautyparlorJSON = businessesJSON.getJSONObject(i);
+                    String name = beautyparlorJSON.getString("name");
+                    String phone = beautyparlorJSON.optString("display_phone", "Phone not available");
+                    String website = beautyparlorJSON.getString("url");
+                    double rating = beautyparlorJSON.getDouble("rating");
+                    String imageUrl = beautyparlorJSON.getString("image_url");
+                    double latitude = beautyparlorJSON.getJSONObject("coordinates").getDouble("latitude");
+                    double longitude = beautyparlorJSON.getJSONObject("coordinates").getDouble("longitude");
+                    ArrayList<String> address = new ArrayList<>();
+                    JSONArray addressJSON = beautyparlorJSON.getJSONObject("location").getJSONArray("display_address");
+                    for (int y = 0; y < addressJSON.length(); y++){
+                        address.add(addressJSON.get(y).toString());
+                    }
+                    ArrayList<String> categories = new ArrayList<>();
+                    JSONArray categoriesJSON = beautyparlorJSON.getJSONArray("categories");
+                    for (int y = 0; y < categoriesJSON.length(); y++){
+                        categories.add(categoriesJSON.getJSONObject(y).getString("title"));
+                    }
+                    BeautyParlor  beautyParlor = new BeautyParlor(name, phone, website, rating,
+                            imageUrl, address, latitude, longitude, categories) ;
+                    beautyParlors.add(beautyParlor);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return beautyParlors;
+    }
 }
 
--- app/src/main/java/com/moringaschool/ebeautyparlor/ui/ParlorActivity.java
+++ app/src/main/java/com/moringaschool/ebeautyparlor/ui/ParlorActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -16,9 +17,11 @@ import android.widget.Toast;
 import com.moringaschool.ebeautyparlor.BeautyParlorArrayAdapter;
 import com.moringaschool.ebeautyparlor.R;
 import com.moringaschool.ebeautyparlor.adapters.ParlorListAdapter;
+import com.moringaschool.ebeautyparlor.models.BeautyParlor;
 import com.moringaschool.ebeautyparlor.services.SalonService;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
@@ -33,19 +36,18 @@ public class ParlorActivity extends AppCompatActivity {
     TextView mLocationTextView;
     @BindView(R.id.listView)
     ListView mListView;
+    public ArrayList<BeautyParlor> mBeautyparlors = new ArrayList<>();
 
-
-
-    private String[] parlor = new String[]{"Beauty point", "Dida beauty parlor", "Sunsession beauty", " Amadivar beauty parlor", " StylePro point",
-            "Superior point", "Brows salon", "farouk beauty", "New look",
-            "Modern hair salon", "Belle beauty", "Yeshi parlor",
-            "Beauty Hub", "Beauty solution", "Ladies bolsos", "Halda naturals", "Lock avenue", "Beauty point", "Jasmine beauty point", "Bella beauty parlor",
-            "Maya salon kenya", "City girl place", "Bruno beauty point", "Emmah beauty parlor", "Natuaral beauty parlor"};
-    private String[] services = new String[]{"Twists", "Short Wavy Bob", "Box braid hairstyles",
-            "Cornrows braids", "Manicure and pedicure", "Soft dreadlocks"
-            , " Facial make up", "Mohawk hair style", "french braid", "ponytail", "curly",
-            "Natural hairstyle", "Box braid", "Dutch braid", "Crotchet braids", "Lemonade braids", "Fishtail braid", "Feed-in braid", "Godness braid", "Braided buns",
-            "Triabal braids", "Fulani braids", "Waterfall braids", "Yarn braids", "Crown braids"};
+//    private String[] parlor = new String[]{"Beauty point", "Dida beauty parlor", "Sunsession beauty", " Amadivar beauty parlor", " StylePro point",
+//            "Superior point", "Brows salon", "farouk beauty", "New look",
+//            "Modern hair salon", "Belle beauty", "Yeshi parlor",
+//            "Beauty Hub", "Beauty solution", "Ladies bolsos", "Halda naturals", "Lock avenue", "Beauty point", "Jasmine beauty point", "Bella beauty parlor",
+//            "Maya salon kenya", "City girl place", "Bruno beauty point", "Emmah beauty parlor", "Natuaral beauty parlor"};
+//    private String[] services = new String[]{"Twists", "Short Wavy Bob", "Box braid hairstyles",
+//            "Cornrows braids", "Manicure and pedicure", "Soft dreadlocks"
+//            , " Facial make up", "Mohawk hair style", "french braid", "ponytail", "curly",
+//            "Natural hairstyle", "Box braid", "Dutch braid", "Crotchet braids", "Lemonade braids", "Fishtail braid", "Feed-in braid", "Godness braid", "Braided buns",
+//            "Triabal braids", "Fulani braids", "Waterfall braids", "Yarn braids", "Crown braids"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,20 +56,11 @@ public class ParlorActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         Intent intent = getIntent();
         String location = intent.getStringExtra("location");
-        BeautyParlorArrayAdapter adapter = new BeautyParlorArrayAdapter(this, android.R.layout.simple_list_item_1, parlor, services);
-        mListView.setAdapter(adapter);
-        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                String parlor = ((TextView) view).getText().toString();
-                Log.v(TAG, "In the onItemClickListener!");
-                Toast.makeText(ParlorActivity.this, parlor, Toast.LENGTH_LONG).show();
-            }
-
-        });
         mLocationTextView.setText("Here are all the beauty parlor near: " + location);
-        getBeautyParlor(location);
+        getBeautyParlor(location) ;
+
     }
+
     private void getBeautyParlor(String location){
         final SalonService salonService = new SalonService();
         salonService.findBeautyParlor(location, new Callback(){
@@ -76,16 +69,31 @@ public class ParlorActivity extends AppCompatActivity {
             public void onFailure(Call call, IOException e){
                 e.printStackTrace();
             }
-
             @Override
             public void onResponse(Call call, Response response) throws IOException {
-                try{
-                    String jsonData = response.body().string();
-                    Log.v(TAG, jsonData);
-                } catch (IOException e){
-                    e.printStackTrace();
-                }
+                 mBeautyparlors = salonService.processResults(response);
+                ParlorActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        String[] beautyparlorNames = new String[mBeautyparlors.size()];
+                        for(int i = 0; i<beautyparlorNames.length;i++){
+                            beautyparlorNames[i]=mBeautyparlors.get(i).getName();
+                        }
+                        ArrayAdapter adapter = new ArrayAdapter(ParlorActivity.this, android.R.layout.simple_list_item_1, beautyparlorNames);
+                        mListView.setAdapter(adapter);
+                        for (BeautyParlor  beautyParlor : mBeautyparlors) {
+                            Log.d(TAG, "Name: " + beautyParlor.getName());
+                            Log.d(TAG, "Phone: " + beautyParlor.getPhone());
+                            Log.d(TAG, "Website: " + beautyParlor.getWebsite());
+                            Log.d(TAG, "Image url: " + beautyParlor.getImageUrl());
+                            Log.d(TAG, "Rating: " + Double.toString(beautyParlor.getRating()));
+                            Log.d(TAG, "Address: " + android.text.TextUtils.join(", ", beautyParlor.getAddress()));
+                            Log.d(TAG, "Categories: " + beautyParlor.getCategories().toString());
+                        }
+                    }
+                });
             }
-        }) ;
+        });
     }
+
 }
