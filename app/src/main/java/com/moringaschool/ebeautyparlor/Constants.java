a5fca1b3ef9c7619c37c135f55f9a8b26fbca4fc
rmogusu <rmogusu123@gmail>
2020-05-31 11:45:12 +0200

    Add a synchronus call.

--- app/src/main/java/com/moringaschool/ebeautyparlor/Constants.java
+++ app/src/main/java/com/moringaschool/ebeautyparlor/Constants.java
@@ -5,9 +5,9 @@ import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 
 public class Constants {
-    public static final String BASE_URL = "https://rose-beautyparlor.herokuapp.com/beautyparlor";
+    public static final String SALON_BASE_URL = "https://rose-beautyparlor.herokuapp.com/beautyparlor";
     //public static final String API_KEY = BuildConfig.API_KEY;
-    public static final String LOCATION_QUERY_PARAMETER = "location";
+    public static final String SALON_LOCATION_QUERY_PARAMETER = "location";
 
 
 }
--- app/src/main/java/com/moringaschool/ebeautyparlor/services/SalonService.java
+++ app/src/main/java/com/moringaschool/ebeautyparlor/services/SalonService.java
@@ -1,12 +1,31 @@
 package com.moringaschool.ebeautyparlor.services;
 
+import com.moringaschool.ebeautyparlor.Constants;
+
+import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
 
 public class SalonService {
     public static void findBeautyParlor(String location, Callback callback) {
 
         OkHttpClient client = new OkHttpClient.Builder()
                 .build();
-    }
+
+        HttpUrl.Builder urlBuilder = HttpUrl.parse(Constants.SALON_BASE_URL).newBuilder();
+        urlBuilder.addQueryParameter(Constants.SALON_LOCATION_QUERY_PARAMETER, location);
+        String url = urlBuilder.build().toString();
+
+        Request request = new Request.Builder()
+                .url(url)
+                //.header("Authorization",Constants.API_KEY)
+                .build();
+
+        Call call = client.newCall(request);
+        call.enqueue(callback);
+   }
+
 }
+
