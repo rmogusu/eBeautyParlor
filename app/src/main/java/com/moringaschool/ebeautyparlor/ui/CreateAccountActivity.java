d39aa33e141f28bb8364d679eb89b532b07d51fb
rmogusu <rmogusu123@gmail>
2020-06-07 15:58:19 +0200

    Add a log in click listener.

--- app/src/main/java/com/moringaschool/ebeautyparlor/ui/CreateAccountActivity.java
+++ app/src/main/java/com/moringaschool/ebeautyparlor/ui/CreateAccountActivity.java
@@ -18,6 +18,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 import com.moringaschool.ebeautyparlor.R;
 
 import butterknife.BindView;
@@ -34,6 +35,7 @@ public class CreateAccountActivity extends AppCompatActivity  implements View.On
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
     private ProgressDialog mAuthProgressDialog;
+    private String mName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -84,6 +86,7 @@ public class CreateAccountActivity extends AppCompatActivity  implements View.On
              final String name = mNameEditText.getText().toString().trim();
              final String email = mEmailEditText.getText().toString().trim();
              String password = mPasswordEditText.getText().toString().trim();
+             //mName = mNameEditText.getText().toString().trim();
              String confirmPassword = mConfirmPasswordEditText.getText().toString().trim();
              boolean validEmail = isValidEmail(email);
              boolean validName = isValidName(name);
@@ -98,7 +101,7 @@ public class CreateAccountActivity extends AppCompatActivity  implements View.On
 
                              if (task.isSuccessful()) {
                                  Log.d(TAG, "Authentication successful");
-
+                                 createFirebaseUserProfile(task.getResult().getUser());
                              } else {
                                  Toast.makeText(CreateAccountActivity.this, "Authentication failed.",
                                          Toast.LENGTH_SHORT).show();
@@ -150,5 +153,23 @@ public class CreateAccountActivity extends AppCompatActivity  implements View.On
             return false;
         }
         return true;
+    }
+    private void createFirebaseUserProfile(final FirebaseUser user) {
+
+        UserProfileChangeRequest addProfileName = new UserProfileChangeRequest.Builder()
+                .setDisplayName(mName)
+                .build();
+
+        user.updateProfile(addProfileName)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, user.getDisplayName());
+                        }
+                    }
+
+                });
     }
 }
--- app/src/main/java/com/moringaschool/ebeautyparlor/ui/LoginActivity.java
+++ app/src/main/java/com/moringaschool/ebeautyparlor/ui/LoginActivity.java
@@ -5,23 +5,39 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.moringaschool.ebeautyparlor.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    public static final String TAG = LoginActivity.class.getSimpleName();
 
-      @BindView(R.id.registerTextView) TextView mRegisterTextView;
+    @BindView(R.id.passwordLoginButton)
+    Button mPasswordLoginButton;
+    @BindView(R.id.emailEditText) EditText mEmailEditText;
+    @BindView(R.id.passwordEditText)
+    EditText mPasswordEditText;
+    @BindView(R.id.registerTextView) TextView mRegisterTextView;
+
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
         ButterKnife.bind(this);
+
         mRegisterTextView.setOnClickListener(this);
+        mAuth = FirebaseAuth.getInstance();
+        mPasswordLoginButton.setOnClickListener(this);
+        mPasswordLoginButton.setOnClickListener(this);
+
     }
 
     @Override
@@ -30,6 +46,21 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             Intent intent = new Intent (LoginActivity.this,CreateAccountActivity.class);
             startActivity(intent);
             finish();
+        }
+        if (view == mPasswordLoginButton) {
+            loginWithPassword();
+        }
+    }
+    private void loginWithPassword() {
+        String email = mEmailEditText.getText().toString().trim();
+        String password = mPasswordEditText.getText().toString().trim();
+        if (email.equals("")) {
+            mEmailEditText.setError("Please enter your email");
+            return;
+        }
+        if (password.equals("")) {
+            mPasswordEditText.setError("Password cannot be blank");
+            return;
         }
     }
 }
